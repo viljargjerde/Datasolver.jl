8ede0caca550da0d85a653b8b4ea73f315261da8
Viljar Helgestad Gjerde <viljargjerde@hotmail.com>
2024-11-24 19:33:36 +0100

    Added LP solver

--- src/Datasolver.jl
+++ src/Datasolver.jl
@@ -2,7 +2,8 @@ module Datasolver
 
 
 include("solver.jl")
-export Dataset, SolveResults, datasolve, my_new_solver, get_final
+export Dataset, SolveResults, datasolve, my_new_solver, get_final, AlgebraicFunction
 include("utils.jl")
-export integrate,create_dataset,connect_in_sequence,create_Φ_bar,get_integration_interval,plot_configuration,discretice_1d_force,get_integration_interval,setup_1d_bar_problem,plot_dataset,plot_dataset,get_rel_diff,convergence_analysis,plot_results
+export integrate,
+	create_dataset, connect_in_sequence, create_Φ_bar, get_integration_interval, plot_configuration, discretice_1d_force, get_integration_interval, setup_1d_bar_problem, plot_dataset, plot_dataset, get_rel_diff, convergence_analysis, plot_results
 end
