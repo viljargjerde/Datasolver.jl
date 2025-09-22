6e39fc4948836afad676d798026150e7c1cfc305
h0a <nguyenhoa18121993@gmail.com>
2025-09-22 14:53:41 +0200

    Fix tests

--- test/test_solver.jl
+++ test/test_solver.jl
@@ -6,6 +6,6 @@
 	push!(solve_result.S, [100.0, 200.0, 300.0])
 
 	final_values = get_final(solve_result)
-	@test final_values == (e = [], E = [0.01, 0.02, 0.03], s = [], S = [100.0, 200.0, 300.0], u = [], λ = [], μ = [], cost = [], equilibrium = [], compatibility = [])
+	@test Dict(pairs(final_values)) == Dict(pairs((e = [], E = [0.01, 0.02, 0.03], s = [], S = [100.0, 200.0, 300.0], u = [], λ = [], μ = [], cost = [], equilibrium = [], compatibility = [], data_idx = Any[], solvetime = Any[])))
 end
 
