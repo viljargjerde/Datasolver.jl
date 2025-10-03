950f32d05824699cda4c12af791c85f5d2f2405c
h0a <nguyenhoa18121993@gmail.com>
2025-10-03 16:03:14 +0200

    update format for NRiter in stored results

--- src/dataset.jl
+++ src/dataset.jl
@@ -41,7 +41,7 @@ Base.@kwdef struct SolveResults
 	solvetime::Vector{Float64} = Vector{Vector{Float64}}()
 	equilibrium::Vector{Vector{Float64}} = Vector{Vector{Float64}}()
 	compatibility::Vector{Vector{Float64}} = Vector{Vector{Float64}}()
-	NRiter::Vector{Int64} = Vector{Vector{Int64}}()
+	NRiter::Vector{Vector{Int64}} = Vector{Vector{Int64}}()
 	ADMiter::Vector{Int64} = Vector{Vector{Int64}}()
 end
 
